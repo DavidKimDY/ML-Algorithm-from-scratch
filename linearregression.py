73cf44429d08385932d0c5315f9694b801cfa8d6
David Kim <kdaeyoub@gmail.com>
2020-04-11 00:13:58 +0900

    Modified somethings i found an error in there..

--- linearregression.py
+++ linearregression.py
@@ -6,7 +6,7 @@ class Simple_Linear_Regression:
     def __init__(self,input,target,epoch=100,learnrate=0.05):
         self.input = input
         self.target = target
-        self.weight = np.zeros(input[0].size+1)
+        self.weight = np.array([np.zeros(input[0].size+1,dtype='float16')])
         self.epoch = epoch
         self.learnrate = learnrate
 
@@ -14,22 +14,21 @@ class Simple_Linear_Regression:
         pass
 
     def output(self,input):
-        output = input.dot(self.weight[1:])+self.weight[0]
-        self.output_=[]
-        self.output_.append(output)
+        output = self.weight[:,1:].dot(input.T)+self.weight[:,0]
+        self.output_=np.array([])
+        self.output_=np.append(self.output_,output)
         return output
 
     def update(self,w,t,input):
+        temp = np.array([[1]])
         n = t.size
         o = self.output(input)
+        delta_w = 2*self.learnrate/n*(t-o).dot(input)
+        delta_w0 = 2*self.learnrate/n*(t-o).sum()
+        w[:,1:] = w[:,1:] + delta_w
+        w[:,0] = w[:,0] + delta_w0
+        #print(w)
 
-        delta_w = 2*self.learnrate/n*(t-o).T.dot(input)
-        delta_w = delta_w.sum()
-        delta_w0 = 2*self.learnrate/n*(t-o)
-        delta_w0 = delta_w0.sum()
-
-        w[1:] = w[1:] + delta_w
-        w[0] = w[0] + delta_w0
 
         return w
 
@@ -37,4 +36,4 @@ class Simple_Linear_Regression:
 
         for _ in range(self.epoch):
             self.weight = self.update(self.weight, self.target, self.input)
-            print(self.weight)
+            #print(self.weight)
